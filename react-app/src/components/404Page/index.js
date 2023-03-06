a891a37fcc724328de61fbfe856f527fc3f2c325
jared <hem.jared@gmail.com>
2023-03-05 23:02:14 -0800

    fixed create form

--- react-app/src/components/404Page/index.js
+++ react-app/src/components/404Page/index.js
@@ -3,7 +3,7 @@ import './error.css'
 
 const Error = () => {
   return (
-    <div className='error-container'>
+    <div className='error-page-container'>
       <h1>404 Page Not Found</h1>
       <h2>The page you are looking for does not exist.</h2>
       <NavLink to='/'>
