36163ef36758d934573293d47afcb87975f00876
jared <hem.jared@gmail.com>
2023-03-25 23:51:51 -0700

    added styling to user navlinks still needs some work

--- react-app/src/components/UserPage/UserNav/index.js
+++ react-app/src/components/UserPage/UserNav/index.js
@@ -1,11 +1,12 @@
 import { NavLink } from "react-router-dom";
+import './userNav.css'
 
 const UserNav = ({userId}) => {
   return (
     // <div>hello from usernav</div>
-    <div>
-      <div><NavLink to={`/user/${userId}/photos`}>Photostream</NavLink></div>
-      <div><NavLink to={`/user/${userId}/albums`}>Albums</NavLink></div>
+    <div className='usernav-navlinks'>
+      <div><NavLink className='user-nav-navlink' to={`/user/${userId}/photos`}>Photostream</NavLink></div>
+      <div className='albums'><NavLink className='user-nav-navlink' to={`/user/${userId}/albums`}>Albums</NavLink></div>
     </div>
   )
 }
